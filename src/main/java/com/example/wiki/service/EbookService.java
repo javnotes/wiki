5165f31ae66c8cf771d288e86866afbcef2afa63
luf <javnotes@gmail.com>
2023-02-28 21:29:02 +0800

    3-8 制作工具类CopyUtil封装BeanUtils

--- src/main/java/com/example/wiki/service/EbookService.java
+++ src/main/java/com/example/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.example.wiki.domain.EbookExample;
 import com.example.wiki.mapper.EbookMapper;
 import com.example.wiki.req.EbookReq;
 import com.example.wiki.resp.EbookResp;
+import com.example.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -27,13 +28,19 @@ public class EbookService {
         criteria.andNameLike("%" + ebookReq.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+        //List<EbookResp> respList = new ArrayList<>();
+        //for (Ebook ebook : ebookList) {
+        //    //EbookResp ebookResp = new EbookResp();
+        //    //BeanUtils.copyProperties(ebook, ebookResp);
+        //    对象复制
+        //    EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //
+        //    respList.add(ebookResp);
+        //}
 
-        return respList;
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
