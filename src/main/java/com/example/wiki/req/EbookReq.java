c10f8bc7316a83576f09a8f4f47e9e07cc941fa0
luf <javnotes@gmail.com>
2023-02-28 21:11:00 +0800

    3-7 封装请求参数和返回参数

---
+++ src/main/java/com/example/wiki/req/EbookReq.java
@@ -0,0 +1,38 @@
+package com.example.wiki.req;
+
+/**
+ * @author luf
+ * @date 2023/02/28 20:36
+ **/
+public class EbookReq {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("EbookReq{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+}
