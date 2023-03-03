399b5c2c436a5741a92319ca06d22bf7972dfa41
luf <javnotes@gmail.com>
2023-03-04 01:07:20 +0800

    4-6 网站首页布局开发

---
+++ web/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <a-button type="primary" ghost>Primary</a-button>
+    <a-button ghost>Default</a-button>
+    <a-button type="dashed" ghost>Dashed</a-button>
+    <a-button danger ghost>Danger</a-button>
+    <a-button type="link" ghost>Link</a-button>
+
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
