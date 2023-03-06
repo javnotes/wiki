b0ce3ccfcbb84b1a14576ab286bbe82f16085a31
luf <javnotes@gmail.com>
2023-03-07 06:59:14 +0800

    5-2 使用 axios 调用后端接口 /ebook/list

---
+++ src/main/java/com/example/wiki/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 允许跨域访问的路径，可以任意配置，可以具体到直接请求路径，也可以是一类请求路径，如：/api/**，这里我们配置为所有请求都支持跨域
+                .allowedOriginPatterns("*") // 允许跨域访问的源，
+                .allowedHeaders(CorsConfiguration.ALL) // 允许请求头，如：X-Token
+                .allowedMethods(CorsConfiguration.ALL) // 允许提交方法，POST、GET等
+                .allowCredentials(true) // 允许cookie跨域，如果要从cookie中获取登录信息，必须加上这句，否则前端拿不到cookie，也就无法判断用户是否登录，也就无法进行权限控制
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+
+}
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,9 +2,6 @@
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
-          mode="inline"
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
       >
         <a-sub-menu key="sub1">
@@ -54,39 +51,17 @@
 </template>
 
 <script lang="ts">
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
+import axios from 'axios';
+
 export default defineComponent({
-  components: {
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
-  },
+  name: 'Home',
   setup() {
-    return {
-      selectedKeys1: ref<string[]>(['2']),
-      selectedKeys2: ref<string[]>(['1']),
-      collapsed: ref<boolean>(false),
-      openKeys: ref<string[]>(['sub1']),
-    };
-  },
+    console.log("setup");
+    axios.get("http://localhost:8880/ebook/list?name=spring").then((response) => {
+      console.log(response);
+    })
+
+  }
 });
 </script>
-<style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
-</style>
