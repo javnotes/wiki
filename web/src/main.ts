92b34b8762ef63151ce5510f0c4735374910d55e
luf <javnotes@gmail.com>
2023-03-07 20:26:03 +0800

    5-6 配置axios拦截器打印请求参数和返回参数

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -8,6 +8,24 @@ import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+
+
+/**
+ * axios拦截器 */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数:', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果:', response);
+    return response;
+}, error => {
+    console.log('返回错误:', error);
+    return Promise.reject(error);
+});
+
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
@@ -20,3 +38,4 @@ for (const i in icons) {
 
 console.log('环境：', process.env.NODE_ENV);
 console.log('服务端：', process.env.VUE_APP_SERVER);
+
