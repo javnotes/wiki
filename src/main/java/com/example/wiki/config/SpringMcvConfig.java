49c332b987ae7b5f5916934d4bc2e9db4170f799
luf <javnotes@gmail.com>
2023-03-07 21:13:50 +0800

    5-9 增加AOP，打印接口耗时、请求参数和返回参数 去掉过滤器和拦截器

--- src/main/java/com/example/wiki/config/SpringMcvConfig.java
+++ src/main/java/com/example/wiki/config/SpringMcvConfig.java
@@ -1,25 +1,25 @@
-package com.example.wiki.config;
-
-import com.example.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-/**
- * @author luf
- * @date 2023/03/07 20:47
- **/
-@Configuration
-public class SpringMcvConfig implements WebMvcConfigurer {
-    @Resource
-    private LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**") // 针对所有请求
-                .excludePathPatterns("/login"); // 不拦截登录请求
-
-    }
-}
+//package com.example.wiki.config;
+//
+//import com.example.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * @author luf
+// * @date 2023/03/07 20:47
+// **/
+//@Configuration
+//public class SpringMcvConfig implements WebMvcConfigurer {
+//    @Resource
+//    private LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**") // 针对所有请求
+//                .excludePathPatterns("/login"); // 不拦截登录请求
+//
+//    }
+//}
