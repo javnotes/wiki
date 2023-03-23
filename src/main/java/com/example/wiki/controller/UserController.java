48895273e9dff6422cdff9c3a41406019d38dc3a
luf <javnotes@gmail.com>
2023-03-23 14:35:03 +0800

    集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/example/wiki/controller/UserController.java
+++ src/main/java/com/example/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.wiki.req.UserLoginReq;
 import com.example.wiki.req.UserQueryReq;
 import com.example.wiki.req.UserResetPasswordReq;
@@ -9,12 +10,17 @@ import com.example.wiki.resp.UserLoginResp;
 import com.example.wiki.resp.UserQueryResp;
 import com.example.wiki.resp.PageResp;
 import com.example.wiki.service.UserService;
+import com.example.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author luf
@@ -23,8 +29,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     /**
      * Spring 会将请求参数自动封装到 UserQueryReq 对象中，这个对象的属性名必须和请求参数名一致，否则无法自动封装，这里的 @Valid 注解是用来校验请求参数的
@@ -93,8 +108,22 @@ public class UserController {
         // 这里使用的是Spring自带的工具类,对密码再次进行MD5加密
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
+        // 调用service层的登录方法，返回用户信息，没有异常就是登录成功
         UserLoginResp userLoginResp = userService.login(req);
+
+        logger.info("用户{}登录成功", userLoginResp.getLoginName());
+        Long token = snowFlake.nextId();
+        logger.info(token.toString());
+
+        // 将token设置到返回结果中，随用户信息返回给前端
+        userLoginResp.setToken(token.toString());
+        // token作为key，用户信息作为value存入redis中
+        // 将token和用户信息存入redis中，设置过期时间为一天
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
+
+
 }
