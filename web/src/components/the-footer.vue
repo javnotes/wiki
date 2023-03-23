23de49ba9519ecb45481114e1ffa704414b543eb
luf <javnotes@gmail.com>
2023-03-23 17:08:35 +0800

    使用vuex保存用户信息，方便其他组件使用。但是刷新页面后vuex中的数据会丢失，所以需要将用户信息保存到SessionStorage中，刷新页面后从SessionStorage中获取用户信息，然后将用户信息保存到vuex中。

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,13 +1,17 @@
 <template>
   <a-layout-footer style="text-align: center">
-    Ant の 电子书
+    Ant の 电子书 <span v-show="user.id">，欢迎：{{ user.name }}</span>
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {computed, defineComponent} from 'vue';
+import store from '@/store';
 
 export default defineComponent({
-  name: 'the-footer'
+  name: 'the-footer',
+  setup() {
+    const user = computed(() => store.state.user);
+  }
 });
 </script>
