a675b9ff3a9a9a0d3ddca5f3be4776e7f662d9f4
luf <javnotes@gmail.com>
2023-03-25 22:11:03 +0800

    增加日志流水号，方便生产运维

--- src/main/java/com/example/wiki/aspect/LogAspect.java
+++ src/main/java/com/example/wiki/aspect/LogAspect.java
@@ -3,6 +3,7 @@ package com.example.wiki.aspect;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
 import com.example.wiki.util.RequestContext;
+import com.example.wiki.util.SnowFlake;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
@@ -12,11 +13,13 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -33,6 +36,9 @@ public class LogAspect {
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 定义一个切点，拦截所有Controller层中的方法，注意：这里的execution表达式是Spring AOP的表达式，不是AspectJ的表达式
      */
@@ -45,6 +51,8 @@ public class LogAspect {
      */
     @Before("controllerPointcut()")
     public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 在线程开始的地方，给日志流水号LOG_ID赋值
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
 
         // 开始打印请求日志
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
