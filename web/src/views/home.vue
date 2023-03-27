64d9194409949ab9a365e7a302d17fe8608adfd3
luf <javnotes@gmail.com>
2023-03-27 12:51:24 +0800

    将欢迎文字提取成组件

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -26,7 +26,7 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎使用</h1>
+        <the-welcome></the-welcome>
       </div>
       <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" grid="{ gutter: 20, column: 3 }"
               :data-source="ebooks">
@@ -75,9 +75,13 @@ import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
 import {Tool} from "@/util/tool";
+import TheWelcome from '@/components/the-welcome.vue';
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    TheWelcome,
+  },
   setup() {
     const ebooks = ref();
 
